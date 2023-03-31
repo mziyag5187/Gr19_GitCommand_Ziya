0ac6a313231eba28eb99560ae201b566ea9012c9
zack <mziyag5187@gmail.com>
2023-03-31 18:22:47 +0300

    my conflict commit on my branch

---
+++ src/test/java/ConflictClass.java
@@ -0,0 +1,13 @@
+public class ConflictClass {
+    public static void main(String[] args) {
+        System.out.println("Conflict from local");
+        System.out.println("Conflict from local");
+        System.out.println("Conflict from local");
+        System.out.println("Conflict from local");
+        System.out.println("Conflict from local");
+        System.out.println("Conflict from local");
+        System.out.println("Conflict from local");
+
+
+    }
+}
