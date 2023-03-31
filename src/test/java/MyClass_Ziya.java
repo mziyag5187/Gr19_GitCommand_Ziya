95e071c41001bbff0cf7bf4bdc3c2c7d92c80d36
zack <mziyag5187@gmail.com>
2023-03-31 17:39:28 +0300

    my second commit

--- src/test/java/MyClass_Ziya.java
+++ src/test/java/MyClass_Ziya.java
@@ -1,5 +1,9 @@
 public class MyClass_Ziya {
     public static void main(String[] args) {
         System.out.println("My first work is done on local");
+
+        System.out.println("My second work is done on local");
+
+
     }
 }
